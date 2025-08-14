c095d4026338ee3cdeef0390ef21d6c130b2539b
Zoey Kaiser <zoeykaiser8@gmail.com>
2025-08-14 15:34:34 +0200

    Initial setup

---
+++ app/app.vue
@@ -0,0 +1,13 @@
+<script lang="ts" setup>
+useHead({ title: 'Side-Clock' })
+</script>
+
+<template>
+  <div class="h-screen bg-green-200 grid place-content-center">
+    <Icon
+      name="svg-spinners:clock"
+      size="200"
+      class="text-green-900"
+    />
+  </div>
+</template>
---
+++ nuxt.config.ts
@@ -0,0 +1,8 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2025-08-12',
+  modules: [
+    '@nuxtjs/tailwindcss',
+    '@nuxt/icon'
+  ],
+})
